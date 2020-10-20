f2046225e02b0d437753173ac9b7fc52f6f7092a
ravi Mishra <ravimishra1017.rm@gmail.com>
2020-10-20 07:23:46 +0530

    Added request service networking layer

---
+++ NewsStar/Code/Network/RequestService.swift
@@ -0,0 +1,38 @@
+//
+//  RequestService.swift
+//  NewsStar
+//
+//  Created by Ravi Mishra on 20/10/20.
+//
+
+import Foundation
+
+final class RequestService {
+    
+    func loadData(urlString: String, session: URLSession = URLSession(configuration: .default), completion: @escaping (Result<Data, ErrorResult>) -> Void) -> URLSessionTask? {
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.kNetwork(string: "Wrong url format")))
+            return nil
+        }
+        ;
+        var request = RequestFactory.request(method: .GET, url: url)
+        
+        if let reachability = Reachability(), !reachability.isReachable {
+            request.cachePolicy = .returnCacheDataDontLoad
+        }
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(.kNetwork(string: "An error occured during request :" + error.localizedDescription)))
+                return
+            }
+            
+            if let data = data {
+                completion(.success(data))
+            }
+        }
+        task.resume()
+        return task
+    }
+}
