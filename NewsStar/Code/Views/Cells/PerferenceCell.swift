b2316cabf483cec8b9c2437d735135dd6ba86ecd
ravi mishra <ravi@zethic.com>
2020-12-16 23:49:59 +0530

    Added preference cell

---
+++ NewsStar/Code/Views/Cells/PerferenceCell.swift
@@ -0,0 +1,18 @@
+//
+//  PerferenceCell.swift
+//  NewsStar
+//
+//  Created by Ravi Mishra on 16/12/20.
+//
+
+import UIKit
+
+class PerferenceCell: UICollectionViewCell {
+    @IBOutlet weak var mainView: SoftUIView!
+    @IBOutlet weak var crossButton: UILabel!
+    @IBOutlet weak var topicName: UILabel!
+
+    override  func awakeFromNib() {
+        
+    }
+}
